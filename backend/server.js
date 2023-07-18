508e29f1abb9d67a77cc0cddf955653f7e2d209e
shad78 <64207889+shad78@users.noreply.github.com>
2023-07-18 14:51:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
